7770e5900daa38eb7ae9d699b8651505d1207439
keshibat <kensukeshibata@gmail.com>
2022-03-28 01:14:10 +0900

    updating our directory page with direcotry component and category item componet

---
+++ src/components/directory/direcotry.component.jsx
@@ -0,0 +1,17 @@
+import CategoryItem from '../category-item/category-item.component'
+
+const Directory = ({categories}) => {
+  return (
+        <div className='directory-container'>
+      {
+        categories.map(({title, id, imageUrl}) => (
+          <div key={id} className='directory-container'>
+            <CategoryItem key={id} title={title} imageUrl={imageUrl} />
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Directory;
