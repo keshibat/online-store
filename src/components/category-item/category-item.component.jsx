63258f891aee5a6bf3abaf2b2f450e9c5cdc33a8
keshibat <kensukeshibata@gmail.com>
2022-03-28 01:36:49 +0900

    fix name

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,16 +1,21 @@
-import './category.styles.scss'
+import './category-item.styles.scss'
 
 
 const CategoryItem = ({ imageUrl, title }) => {
   return (
-          <div className='category-container'>
-            <div  className='background-image' style={{ backgroundImage: `url(${imageUrl})`}} />
-            <div className='category-body-container'>
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
+    <div className='category-container'>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
   )
 }
 
-export default CategoryItem
+export default CategoryItem;
