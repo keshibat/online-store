44cb2e584e06d723b4be9b4b8c233737372cc7e4
keshibat <kensukeshibata@gmail.com>
2022-04-13 11:01:00 +0900

    added products context provider

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context'
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
+      <UserProvider>
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
+     </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
