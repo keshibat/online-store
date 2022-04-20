3d919fd8428f04145a2896c595a55e881f94793e
keshibat <kensukeshibata@gmail.com>
2022-04-20 18:23:06 +0900

    addCollectionAndDocument

--- src/utils/firebase/firebase.utlis.js
+++ src/utils/firebase/firebase.utlis.js
@@ -6,13 +6,15 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from 'firebase/auth';
 import {
   getFirestore,
   doc,
   getDoc,
   setDoc,
+  collection,
+  writeBatch,
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -38,6 +40,24 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  field
+  ) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+
+}
 
 export const createUserDocumentFromAuth = async(userAuth, additionalInformation={displayName: 'test'}) => {
   if(!userAuth) return;
