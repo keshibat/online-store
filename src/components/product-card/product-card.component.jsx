232af8d150e0ebee36ad3dbfbe0ca4ac5537a868
keshibat <kensukeshibata@gmail.com>
2022-05-11 11:47:09 +0900

    fix bug product card compoennt

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -12,13 +12,12 @@ import {
   Price,
 } from './product-card.styles';
 
-
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
 
-  const addProductToCart = () => dispatch(addItemToCart(product));
+  const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
 
   return (
     <ProductCartContainer>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,3 @@
-
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -6,22 +5,28 @@ import { useSelector } from 'react-redux';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { selectIsCartOpen } from '../../store/cart/cart.selector'
-import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import { selectCurrentUser } from '../../store/user/user.selector';
+import { selectIsCartOpen } from '../../store/cart/cart.selector';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utlis';
 
-
-import { NavigationContainer, LogoContainer, NavLinks,  NavLink} from './navigation.styles';
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from './navigation.styles';
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+
   return (
     <Fragment>
       <NavigationContainer>
         <LogoContainer to='/'>
-          <CrownLogo />
+          <CrwnLogo className='logo' />
         </LogoContainer>
         <NavLinks>
           <NavLink to='/shop'>SHOP</NavLink>
@@ -39,7 +44,7 @@ const Navigation = () => {
       </NavigationContainer>
       <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
 export default Navigation;
