4a55f58159c0a434af4d253fdd851fec1dbc38ca
keshibat <kensukeshibata@gmail.com>
2022-05-20 17:22:31 +0900

    Typing user action creators

--- src/store/categories/category.types.ts
+++ src/store/categories/category.types.ts
@@ -5,3 +5,17 @@ export enum CATEGORIES_ACTION_TYPES = {
 };
 
 
+export type CategoryItem = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+}
+
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[]
+}
+
